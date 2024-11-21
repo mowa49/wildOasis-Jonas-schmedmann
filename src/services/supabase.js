35670180e1a40dd15f1904a8a8897e577098bb45
mowa49 <mostafafaraji94@gmail.com>
2024-11-21 15:38:49 +0100

    deploy fix3

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -4,10 +4,10 @@ import { createClient } from "@supabase/supabase-js";
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 export const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 console.log(import.meta.env.VITE_TEST_VAR);
-if (!supabaseUrl ) {
+if (!supabaseUrl) {
   throw new Error("Missing Supabase URL variables");
 }
-if (|| !supabaseKey) {
+if (!supabaseKey) {
   throw new Error("Missing Supabase KEY ");
 }
 
