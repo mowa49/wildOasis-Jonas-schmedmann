234e1568866adb74737a0a9412362f445672b730
mowa49 <mostafafaraji94@gmail.com>
2024-11-21 14:12:29 +0100

    changed keys name

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 // src/services/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-export const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabaseUrl = import.meta.env.SUPABASE_URL;
+export const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
 console.log(import.meta.env.VITE_TEST_VAR);
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Missing Supabase environment variables");
