6304571b9acd25691c10104a4d733476511de1be
mowa49 <mostafafaraji94@gmail.com>
2024-11-21 15:09:57 +0100

    deploy fix

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,8 +8,8 @@ import useLogin from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("a@a.com");
-  const [password, setPassword] = useState("a");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -3,18 +3,19 @@ import { useEffect, useRef } from "react";
 export function useOutsideClick({ handler, listenCapturing = true }) {
   const ref = useRef();
   useEffect(
-    function () {
-      function handleClick(e) {
-        if (ref.current && !ref.current.contains(e.target)) {
-          console.log("click outside");
-          handler();
+    () =>
+      function () {
+        function handleClick(e) {
+          if (ref.current && !ref.current.contains(e.target)) {
+            console.log("click outside");
+            handler();
+          }
         }
-      }
-      document.addEventListener("click", handleClick, listenCapturing);
+        document.addEventListener("click", handleClick, listenCapturing);
 
-      return () =>
-        document.removeEventListener("click", handleClick, listenCapturing);
-    },
+        return () =>
+          document.removeEventListener("click", handleClick, listenCapturing);
+      },
     [handler]
   );
   return ref;
